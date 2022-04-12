8c1179ef2b3afc1c4e5ea05c347fe853eb3556b2
MrSlipperz <90514272+MrSlipperz@users.noreply.github.com>
2022-04-12 12:01:41 +0200

    Necessary folders

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smart_E.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smart_E.Data;
+
+namespace Smart_E.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<ProfileController> _logger;
+        private readonly ApplicationDbContext _context;
+
+
+        public ProfileController(ILogger<ProfileController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
