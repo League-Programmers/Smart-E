a5f4a0db66aec2467406818a6fccf96704ea6e52
caelan <caelan@techla.co.za>
2022-04-20 21:12:55 +0200

    update

---
+++ Data/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Smart_E.Models
+{
+    public class User : IdentityUser
+    {
+        [PersonalData]
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [PersonalData]
+        [Required]
+        public string LastName { get; set; }
+
+        [PersonalData]
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Role { get; set; }
+
+    }
+}
