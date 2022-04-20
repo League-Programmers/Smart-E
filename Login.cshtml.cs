f3284e7c312e7505f959b3df720b391b88f2b331
caelan <caelan@techla.co.za>
2022-04-20 20:41:34 +0200

    update

--- Login.cshtml.cs
+++ Login.cshtml.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using ONT3PROJECT_2022.Areas.Identity.Data;
 
-namespace ONT3PROJECT_2022.Areas.Identity.Pages.Account
+namespace Smart_E
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
