67bf634c4fb6b27be267fc886cc460154521b425
caelan <caelan@techla.co.za>
2022-04-29 10:46:08 +0200

    update

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+<<<<<<< HEAD
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
@@ -24,3 +25,28 @@ app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 app.Run();
+=======
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+        public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+
+        public static IWebHostBuilder CreateDefaultBuilder(string[] args)
+        {
+            var builder = new WebHostBuilder().UseKestrel().UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    /* setup config */
+                }).ConfigureLogging((hostingContext, logging) =>
+                {
+                    /* setup logging */
+                }).UseIISIntegration();
+            return builder;
+        }
+       
+    }
+}
+>>>>>>> parent of 303b2ec (update)
