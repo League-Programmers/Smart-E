b270019da3e99e153a1a7aad5010d13eca6710b6
caelan <caelan@techla.co.za>
2022-05-10 10:43:57 +0200

    update

---
+++ Models/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace Smart_E.Models
+{
+    public class CourseViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string TeacherName { get; set; }
+     
+    }
+}
