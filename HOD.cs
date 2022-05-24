74bd5cffc051de0a17ebe5c060cf156b90b4b0f6
YVR01 <90517287+YVR01@users.noreply.github.com>
2022-05-24 12:58:53 +0200

    Add files via upload

---
+++ HOD.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_E.Data
+{
+    public class HOD
+    {
+        [Key]
+        public int HODID { get; set; }
+
+        [Required]
+        public string HODName { get; set; }
+
+        [Required]
+        public string HODSurname { get; set; }
+
+        [Required]
+        public string HODEmail { get; set; }
+
+        [Required]
+        public int HODPhoneNumber { get; set; }
+
+        //[Required]
+        //public int DepartmentID { get; set; }
+    }
+}
---
+++ HODController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smart_E.Data;
+
+namespace Smart_E.Controllers
+{
+    public class HODController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public HODController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult HOD()
+        {
+            return View();
+        }
+        
+        public IActionResult GetStudentsReport()
+        {
+            IEnumerable<Student> objList = _context.Students; 
+            return View(objList);
+        }
+
+
+    }
+}
