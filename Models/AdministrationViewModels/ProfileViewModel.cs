bd4baa307c69199110c75c8f273cce9d0c92e75d
caelan <caelan@techla.co.za>
2022-05-26 00:28:47 +0200

    update

--- Models/AdministrationViewModels/ProfileViewModel.cs
+++ Models/AdministrationViewModels/ProfileViewModel.cs
@@ -17,15 +17,11 @@
 
         public string JobDescription { get; set; }
 
-        public string ContactInformation { get; set; }
-
-        public string CompanyName { get; set; }
-
         public string ProfilePictureUrl { get; set; }
 
         public string ProfilePictureFileName { get; set; }
 
+        public string PhoneNumber { get; set; }
 
-        public bool Status { get; set; }
     }
 }
---
+++ Models/Profile/UpdateUserPostModal.cs
@@ -0,0 +1,12 @@
+﻿namespace Smart_E.Models.Profile
+{
+    public class UpdateUserPostModal
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool Gender { get; set; }
+    }
+}
