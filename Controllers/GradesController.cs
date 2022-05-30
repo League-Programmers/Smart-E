35fea962ae37fcbcf5d7c2d21605107c2c2c0f6e
caelan <caelan@techla.co.za>
2022-05-30 20:36:21 +0200

    update

---
+++ Controllers/GradesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smart_E.Controllers
+{
+    public class GradesController : Controller
+    {
+        public IActionResult Grades()
+        {
+            return View();
+        }
+    }
+}
