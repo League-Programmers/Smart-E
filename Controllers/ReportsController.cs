e5be983fb57564d9344635a0321a7d038784c350
caelan <caelan@techla.co.za>
2022-05-31 10:24:37 +0200

    update

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smart_E.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
