704e700fff44513becdd844666e59e4622b355f9
MrSlipperz <90514272+MrSlipperz@users.noreply.github.com>
2022-08-05 12:58:13 +0200

    Emailing

---
+++ Models/EmailService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Smart_E.Models
+{
+    public class EmailService : IEmailService
+    {
+        private const string templatePath = @"EmailTemplate/{0}.html";
+        private readonly SMTPConfigModel _smtpConfig;
+
+        public EmailService(IOptions<SMTPConfigModel> smtpConfig)
+        {
+            _smtpConfig = smtpConfig.Value;
+        }
+
+        public async Task SendTestEmail(UserEmailOptions userEmailOptions)
+        {
+            userEmailOptions.Subject = UpdatePlaceHolders("HELLO {{UserName}}, This is test email subject from League Programmers", userEmailOptions.PlaceHolders);
+            userEmailOptions.Body = UpdatePlaceHolders(GetEmailBody("TestEmail"), userEmailOptions.PlaceHolders);
+
+            await SendEmail(userEmailOptions);
+        }
+
+        private async Task SendEmail(UserEmailOptions userEmailOptions)
+        {
+            MailMessage mail = new MailMessage
+            {
+                Subject = userEmailOptions.Subject,
+                Body = userEmailOptions.Body,
+                From = new MailAddress(_smtpConfig.SenderAddress, _smtpConfig.SenderDisplayName),
+                IsBodyHtml = _smtpConfig.IsBodyHTML
+            };
+
+            foreach (var toEmail in userEmailOptions.ToEmails)
+            {
+                mail.To.Add(toEmail);
+            }
+
+            NetworkCredential networkCredential = new NetworkCredential(_smtpConfig.UserName, _smtpConfig.Password);
+
+            SmtpClient smtpClient = new SmtpClient
+            {
+                Host = _smtpConfig.Host,
+                Port = _smtpConfig.Port,
+                EnableSsl = _smtpConfig.EnableSSL,
+                UseDefaultCredentials = _smtpConfig.UseDefaultCredentials,
+                Credentials = networkCredential
+            };
+            mail.BodyEncoding = Encoding.Default;
+            await smtpClient.SendMailAsync(mail);
+
+        }
+        private string GetEmailBody(string templateName)
+        {
+            var body = File.ReadAllText(string.Format(templatePath, templateName));
+            return body;
+        }
+
+        private string UpdatePlaceHolders( string txt, List<KeyValuePair<string,string>> keyValuePairs)
+        {
+            if(!string.IsNullOrEmpty(txt) && keyValuePairs != null)
+            {
+                foreach(var placeHolder in keyValuePairs)
+                {
+                    if (txt.Contains(placeHolder.Key))
+                    {
+                        txt = txt.Replace(placeHolder.Key, placeHolder.Value);
+                    }
+                }
+            }
+
+            return txt;
+        }
+    }
+}
---
+++ Models/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Smart_E.Models
+{
+    public interface IEmailService
+    {
+        Task SendTestEmail(UserEmailOptions userEmailOptions);
+    }
+}
