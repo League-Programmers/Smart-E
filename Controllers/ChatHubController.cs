6d56b2647cfff9d8cf36e1d872f685cafc3662eb
caelan <caelan@techla.co.za>
2022-08-14 23:39:35 +0200

    comment section

---
+++ Controllers/ChatHubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smart_E.Controllers
+{
+    public class ChatHubController : Controller
+    {
+        public IActionResult ChatHub()
+        {
+            return View();
+        }
+    }
+}
