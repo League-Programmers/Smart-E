4a2aeee3f7e0b01add56c8ffbb399b944c5e76c0
caelan <caelan@techla.co.za>
2022-08-14 21:47:19 +0200

    update
    MyChildren controller

---
+++ Controllers/MyChildController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Smart_E.Data;
+
+namespace Smart_E.Controllers
+{
+    public class MyChildController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MyChildController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult MyChild()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult>GetChildren()
+        {
+            var children = await (
+                from u in _context.Users
+                join ur in _context.UserRoles
+                    on u.Id equals ur.UserId
+                join r in _context.Roles
+                    on ur.RoleId equals r.Id
+                where r.Name == "Student"
+                select new
+                {
+                    Id = u.Id,
+                    Name = u.FirstName + " "+ u.LastName,
+                    Email = u.Email,
+
+                }).ToListAsync();
+
+            return Json(children);
+
+        }
+    }
+}
