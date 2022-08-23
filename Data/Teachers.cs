6132472879fa1f37171fff96350f8415908c9b06
MrSlipperz <90514272+MrSlipperz@users.noreply.github.com>
2022-08-23 13:39:41 +0200

    Added Teacher.cs

---
+++ Data/Teachers.cs
@@ -0,0 +1,9 @@
+﻿namespace Smart_E.Data
+{
+    public class Teachers
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
