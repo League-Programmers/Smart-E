4127c261186635bfaf8f0ae1254405f2b6901027
caelan <caelan@techla.co.za>
2022-09-09 09:27:37 +0200

    update

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -17,17 +17,31 @@ namespace Smart_E.Controllers
         }
         public async Task<IActionResult> GetAllUsers()
         {
-            var teachers = await (
+            var users = await (
                 from c in _context.Users
+                join ur in _context.UserRoles
+                    on c.Id equals ur.UserId
+                    join r in _context.Roles
+                    on ur.RoleId equals r.Id
                 select new
                 {
+                    Id = c.Id,
                     Name = c.FirstName + " "+ c.LastName,
-                    Email = c.Email
-
+                    Email = c.Email,
+                    Status = c.Status,
+                    Role = r.Name
 
                 }).ToListAsync();
 
-            return Json(teachers);
+            return Json(users);
+        }
+
+        public async Task<IActionResult> GetUser([FromQuery]string id)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.Id == id);
+               
+
+            return Json(user);
         }
     }
 }
