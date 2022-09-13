42a4437d0e21d119b7bc78722ae979883d5436f1
caelan <caelan@techla.co.za>
2022-09-14 01:23:59 +0200

    update

--- Models/AdministrationViewModels/ProfileViewModel.cs
+++ Models/AdministrationViewModels/ProfileViewModel.cs
@@ -13,9 +13,9 @@
         public  string Email { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
-        public int Year { get; set; }
+        public DateTime Year { get; set; }
 
-
+        public string School { get; set; }
         public Guid QualificationId { get; set; }
         public string PhoneNumber { get; set; }
 
