ab0602a8dbdd6074fce99f7b47923b68cc8200b6
caelan <caelan@techla.co.za>
2022-10-09 11:09:28 +0200

    update

---
+++ Models/Roles/RoleModification.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Smart_E.Models.Roles
+{
+    public class RoleModification
+    {
+        [Required]
+        public string RoleName { get; set; }
+
+        public string RoleId { get; set; }
+
+        public string[]? AddIds { get; set; }
+
+        public string[]? DeleteIds { get; set; }
+    }
+}
